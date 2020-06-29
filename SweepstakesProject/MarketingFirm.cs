01b06313078e8225c06b9f540530ae8e60c4a5f1
megancindric <megancindric@gmail.com>
2020-06-29 10:16:50 -0500

    Added classes for Stack, Queue, & Sweepstakes manager, contestant, user interface, & marketingfirm.  Added notes to each class's variables, methods, & ctor

---
+++ SweepstakesProject/MarketingFirm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SweepstakesProject
+{
+    class MarketingFirm
+    {
+        //member variables
+
+        //ctor
+
+        //member methods
+    }
+}
---
+++ SweepstakesProject/UserInterface.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SweepstakesProject
+{
+    interface UserInterface
+    {
+    }
+}
