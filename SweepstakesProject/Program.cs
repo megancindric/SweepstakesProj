a12874e7be793052cc834409d4b39296b72c9601
megancindric <megancindric@gmail.com>
2020-06-29 09:52:34 -0500

    Init

---
+++ SweepstakesProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SweepstakesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
